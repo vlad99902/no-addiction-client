c5f1f10336d90c3dc6ed8f0288b9eb7e776c036f
xylb-pro <churakin.kirill@gmail.com>
2021-01-18 04:15:24 +0300

    q

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          KekW <code>src/App.tsx</code> and save to reload.
+          Kekw <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
